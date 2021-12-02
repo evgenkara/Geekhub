5f7793311458561bd97d5a20346a364a3afe6e58
greyom <50679401+greyom@users.noreply.github.com>
2021-12-02 16:04:34 +0200

    Edited 6.py

--- HT_5/6.py
+++ HT_5/6.py
@@ -3,19 +3,26 @@
 P.S. Повинен вертатись генератор.
 """
 
-
-def myrange(start, stop, step = 1):
+def myrange(start, stop = None, step = 1):
+    if stop == None:
+        stop = start; start = 0
     if step > 0:
-        while start < stop:
-            yield start
-            start += step
+        if start > stop:
+            raise ValueError
+        else:
+            while start < stop:
+                yield start
+                start += step
     if step < 0:
-        while start > stop:
-            yield start
-            start += step
+        if start < stop:
+            raise ValueError
+        else:
+            while start > stop:
+                yield start
+                start += step
     elif step == 0:
         raise ValueError
 
-ran = myrange(15, 0, -1)
+ran = myrange(30)
 for i in ran:
     print(i)
