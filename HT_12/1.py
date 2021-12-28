f13e49b5d65f081e769e40cf75da61dbb3659db8
greyom <50679401+greyom@users.noreply.github.com>
2021-12-28 21:50:33 +0200

    Edited 1.py

--- HT_12/1.py
+++ HT_12/1.py
@@ -13,6 +13,7 @@ def scrape_author(url):
     author_loc = author_soup.select_one(".author-born-location").text
     author_born = author_date + " " + author_loc
     author_info = author_soup.select_one(".author-description").text
+    print(author_soup.select_one(".author-title").text)
     return author_born, author_info
 
 
@@ -24,7 +25,7 @@ def to_csv(data_list):
         writer.writerows(data_list)
 
 
-def scrape(url, data_list):
+def scrape_page(url, data_list):
     con = sqlite3.connect("Quotes.db")
     cur = con.cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS quotes (Quote text, Author text)''')
@@ -37,10 +38,20 @@ def scrape(url, data_list):
         text = quote.find('span', {'class': 'text'}).text
         text = text.replace(";", ":")
         author = quote.find('small', {'class': 'author'}).text
-        ref = quote.select_one("a").get('href')
-        author_born = scrape_author('http://quotes.toscrape.com' + ref)[0]
-        author_info = scrape_author('http://quotes.toscrape.com' + ref)[1].strip()
-        data_list.append({"Quote": text, "Author": author, "Author_birth": author_born, "Author_info": author_info})
+        scrapped_author = {}
+        for aut in data_list:
+            if aut["Author"] == author:
+                scrapped_author = aut
+        if len(scrapped_author) > 0:
+            author_born = scrapped_author["Author_birth"]
+            author_info = scrapped_author["Author_info"]
+        else:
+            ref = quote.select_one("a").get('href')
+            author_data = scrape_author('http://quotes.toscrape.com' + ref)
+            author_born = author_data[0]
+            author_info = author_data[1].strip()
+        data_list.append({"Quote": text, "Author": author, "Author_birth": author_born,
+                          "Author_info": author_info})
         cur.execute('''INSERT INTO quotes (Quote, Author) VALUES (?,?)''',
                     (text, author))
         cur.execute('''INSERT OR IGNORE INTO authors VALUES (?,?,?)''',
@@ -48,13 +59,19 @@ def scrape(url, data_list):
     con.commit()
     ref_next = soup.select_one('.next a')
     if ref_next:
-        next_url = ref_next.get('href')
+        return ref_next
+
+
+def scrapper():
+    data = []
+    start_url = "http://quotes.toscrape.com/page/1/"
+    scrape = scrape_page(start_url, data)
+    while scrape is not None:
+        next_url = scrape.get('href')
         time.sleep(0.5)
-        scrape(f"http://quotes.toscrape.com{next_url}", data_list)
+        scrape = scrape_page(f"http://quotes.toscrape.com{next_url}", data)
+    to_csv(data)
+    print("Scrapping completed")
 
 
-data = []
-start_url = "http://quotes.toscrape.com/page/1/"
-scrape(start_url, data)
-to_csv(data)
-print("Scrapping completed")
+scrapper()
