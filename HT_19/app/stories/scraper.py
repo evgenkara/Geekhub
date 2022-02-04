8092edf33349150a6f06f7e88becaf3d24ed5310
greyom <50679401+greyom@users.noreply.github.com>
2022-02-04 12:04:18 +0200

    Updated scraper

--- HT_19/app/stories/scraper.py
+++ HT_19/app/stories/scraper.py
@@ -9,24 +9,22 @@ class Scraper(object):
 
     def get_item(self, post_id, category):
         req_item = dict(requests.get(f"https://hacker-news.firebaseio.com/v0/item/{post_id}.json").json())
-        item_category = self.categories_available[category]
-        item_id = req_item['id']
-        try:
-            if item_category.objects.get(story_id=item_id):
-                print(f"Already in base: {item_id}")
-            else:
-                return req_item
-        except ObjectDoesNotExist:
-            return req_item
+        return req_item
 
     def start(self, category_given):
         posts = []
         r = requests.get(f"https://hacker-news.firebaseio.com/v0/{category_given}.json")
         for item in r.json():
             try:
-                scraped_item = self.get_item(item, category_given)
-                if scraped_item is not None:
+                item_category = self.categories_available[category_given]
+                if item_category.objects.get(story_id=item):
+                    print(f"Already in base: {item}")
+                else:
+                    scraped_item = self.get_item(item, category_given)
                     posts.append(scraped_item)
+            except ObjectDoesNotExist:
+                scraped_item = self.get_item(item, category_given)
+                posts.append(scraped_item)
             except TypeError:
                 continue
         return posts
