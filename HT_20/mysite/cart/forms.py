a613f8b92c1c45ec020b1102425cdfd4528ed887
greyom <50679401+greyom@users.noreply.github.com>
2022-02-10 18:35:52 +0200

    updated cart.form

--- HT_20/mysite/cart/forms.py
+++ HT_20/mysite/cart/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from shop.models import Product
 
 
 class CartAddProductForm(forms.Form):
@@ -12,18 +11,3 @@ class CartAddProductForm(forms.Form):
         if quant:
             quant_choices = [(i, str(i)) for i in range(1, quant['quant'] + 1)]
             self.fields['quantity'].choices = quant_choices
-
-
-
-
-# class CartAddProductForm(forms.Form):
-#     quantity = forms.TypedChoiceField(choices=[], coerce=int)
-#     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
-#
-#     def __init__(self, product, product_id, *args, **kwargs):
-#         super(CartAddProductForm, self).__init__(*args, **kwargs)
-#
-#         obj = Product.objects.get(pk=product_id)
-#         stock = obj.stock
-#         quant_choices = [(i, str(i)) for i in range(1, stock+1)]
-#         self.fields['quantity'].choices = quant_choices
