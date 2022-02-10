115e8daf4fb5a10d2ea62e428a6aba12bd00f48e
greyom <50679401+greyom@users.noreply.github.com>
2022-02-10 04:30:53 +0200

    Updated mysite

---
+++ HT_20/mysite/cart/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from shop.models import Product
+
+
+class CartAddProductForm(forms.Form):
+    quantity = forms.TypedChoiceField(choices=[], coerce=int)
+    update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+    def __init__(self, product_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        obj = Product.objects.get(pk=product_id)
+        stock = obj.stock
+        quant_choices = [(i, str(i)) for i in range(1, stock+1)]
+        self.fields['quantity'].choices = quant_choices
